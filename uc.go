cb747cb86af3dc93065d01e382635f1ae6eeb317
Rob Cameron <rwcameron@gmail.com>
2015-06-28 20:14:48 -0700

    update

--- uc.go
+++ uc.go
@@ -1,13 +1,17 @@
 package main
 
-import "testing"
-import "time"
+import (
+	"log"
+	"time"
+)
 
-func BasicUnicornManager(t *testing.T) {
+func main() {
 	um := UnicornManager{}
 	um.init()
+	log.Println("Init Unicorn")
 	for i := 0; i < 64; i++ {
 		p := &Pixel{pos: uint(i), r: 255, b: 255, g: 255}
+		log.Println("Pixel", p)
 		um.SetPixelColor(p)
 		time.Sleep(1 * time.Second)
 	}
